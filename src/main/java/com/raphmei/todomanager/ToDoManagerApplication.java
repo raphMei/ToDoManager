8139aaca0521e38d694bc901ae1a253ed12fa856
Raphael Meimoun <raphael.meimoun95@gmail.com>
2025-03-27 12:15:05 +0100

    first commit

---
+++ src/main/java/com/raphmei/todomanager/ToDoManagerApplication.java
@@ -0,0 +1,13 @@
+package com.raphmei.todomanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToDoManagerApplication.class, args);
+    }
+
+}
